7852ffd4a3bce411d3823ee07a1d45452c230273
devfromweiren <162145909+devfromweiren@users.noreply.github.com>
2024-03-12 09:59:01 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,32 +1,44 @@
+let AttributeRestraint;
 
-
-$(document).ready(function(){
-    $('.pokemonball').on('click',function(){
+$(document).ready(function () {
+    $('.pokemonball').on('click', function () {
         $(this).toggleClass('jump')
         $('.tool-wrap').toggleClass('show')
     })
 
-    $('.tool-wrap > .battle').on('click',function(){
-        location.href='battle.html'
+    $('.tool-wrap > .battle').on('click', function () {
+        location.href = 'battle.html'
     })
 
-    $('.pick-wrap .attribute').on('click',function(){
+    $('.pick-wrap .attribute').on('click', function () {
         $(this).toggleClass('picker')
 
         Battle()
+    })
 
+    $.get('./json/AttributeRestraint.json',function(data,status){
+        console.log(data)
     })
 })
 
+let attributeSort = ['normal', 'fire', 'water', 'elec', 'grass', 'ice', 'fighter', 'poison', 'ground', 'fly', 'super', 'insect', 'rock', 'ghost', 'dragon', 'dark', 'iron', 'globin']
+let efficient = 1.60
+let invalid = 0.625
+let fail = 0.391
 
-function Battle(){
-    let elements = [] ;
+function Battle() {
+    let elements = [];
 
-    $('.picker-wrap .picker').each(function(index){
-        if(!elements.includes($(this).text())){
-            elements.push($(this).text())
+    $('.picker-wrap .picker').each(function (index) {
+        let value = $(this).data('value')
+        if (!elements.includes(value)) {
+            elements.push(value)
         }
     })
-    
+
+    $.get('../json/AttributeRestraint.json',function(data,status){
+        
+    })
+
     console.log(elements)
 }
